52b43edc3d5a81f736af259b972b98f385f07c95
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-12-12 00:43:43 +0600

    Implemented the Authentication

--- src/cfehome/api.py
+++ src/cfehome/api.py
@@ -1,16 +1,22 @@
 from ninja import NinjaAPI, Schema
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
+from ninja_extra import NinjaExtraAPI
 
-api = NinjaAPI()
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
+
 
 class UserSchema(Schema):
     username: str
-    is_authenticated: bool
+    # is_authenticated: bool
+    email: str = None
 
 @api.get("/hello")
 def hello(request):
     print(request)
     return "Hello World"
 
-@api.get("/me", response=UserSchema)
+@api.get("/me", response=UserSchema, auth=JWTAuth())
 def me(request):
     return request.user
